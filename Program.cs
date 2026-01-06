11476d754698cd71dcb1abbe5b63f7b7a5c8c401
kathborges <katherineborgescordeiro@gmail.com>
2026-01-06 18:22:12 -0300

    [200~

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste Nokia:");
+Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "123456", memoria: 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Teste Iphone:");
+Iphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 13", imei: "987654", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
